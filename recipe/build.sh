da16fd27f00adabe43298526b855e2286c0243b8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-06-26 15:16:28 -0700

    Update build recipe and git commit hash

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,19 +7,23 @@ perl -pi -e "s#(/[^\s]*?lib)/lib([^\s]+).la#-L\$1 -l\$2#g" ${PREFIX}/lib/*.la
 
 mkdir build && cd build
 
+export LDFLAGS="-L$PREFIX/lib"
+
 # Make libraries
-cmake                                          \
-  -DCMAKE_BUILD_TYPE=Release                   \
-  -DCMAKE_CXX_FLAGS='-g -O3 -std=c++11  -fPIC' \
-  -DCMAKE_PREFIX_PATH=${PREFIX}                \
-  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}        \
-  -DBoost_INCLUDE_DIR:PATH=${PREFIX}/include   \
-  -DTIFF_INCLUDE_DIR:PATH=${PREFIX}/include    \
-  -DBUILD_SHARED_LIBS=ON                       \
-  -DCMAKE_VERBOSE_MAKEFILE=ON                  \
-  -DBUILD_SHARED_LIBS=ON                       \
-  -DBUILD_TESTING=OFF                          \
-  -DBUILD_DOCUMENTATION=OFF                    \
+cmake                                                  \
+  -DCMAKE_BUILD_TYPE=Release                           \
+  -DCMAKE_CXX_FLAGS="-g -O3 -std=c++11 -fPIC $LDFLAGS" \
+  -DCMAKE_PREFIX_PATH=${PREFIX}                        \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}                \
+  -DBoost_INCLUDE_DIR:PATH=${PREFIX}/include           \
+  -DTIFF_INCLUDE_DIR:PATH=${PREFIX}/include            \
+  -DBUILD_SHARED_LIBS=ON                               \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                          \
+  -DBUILD_SHARED_LIBS=ON                               \
+  -DBUILD_TESTING=OFF                                  \
+  -DBUILD_DOCUMENTATION=OFF                            \
+  -DTBB_LIBRARIES=${PREFIX}/lib/libtbb.so.2            \
+  -DTBB_MALLOC_LIB=${PREFIX}/lib/libtbbmalloc.so.2     \
   ${SRC_DIR}
 
 make -j $CPU_COUNT
